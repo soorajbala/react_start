d6bc04667ba9a614a1980aa6a24c1b0751a71cbe
SoorajBala <sooraj.bala@gmail.com>
2017-12-13 20:38:26 +0530

    code cleanup using jsx but create and error so use babel

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const { createElement } = React
 const { render } = ReactDOM
-const style = { 
-	                  backgroundColor: 'red',
-	                  padding : '10px'
-                   }
-const title = createElement(
-	'h1',
-	{ id: 'title', className: 'header', style: style },
-	'Hello World'
-)
 
-render(
-  title,
-  document.getElementById('react-conatiner') 
+render( <
+    h1 className = 'header'
+    id = 'title'
+    style = {
+        {
+            backgroundColor: 'red',
+            padding: '10px'
+        }
+    } >
+    Sweet rascal <
+    /h1>,
+
 )
